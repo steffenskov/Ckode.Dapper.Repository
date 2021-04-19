d24b640f4ed8887b66b52b0b6ff92997e15f5f28
Steffen Skov <steffen@ckode.dk>
2021-04-19 21:38:46 +0200

    Got docker working for Northwind test DB

--- Docker setup/entrypoint.sh
+++
@@ -1,1 +0,0 @@
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P SqlServer2019 -i /usr/share/instnwnd.sql
--- Docker setup/run.sh
+++ Docker setup/run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 sudo docker run -p 1433:1433 --name dapper-repo-sql -h dapper-repo-sql -d dapper-repo-sql:latest
-sudo docker exec -it dapper-repo-sql "bash"
