484d426d08dfa9adaff56a15f7134b32c934f1d7
Steffen Skov <steffen@ckode.dk>
2021-04-25 21:48:45 +0200

    Couple of file renamings.

--- Tests/Ckode.Dapper.Repository.IntegrationTests/Entities/CategoryEntity.cs
+++ Tests/Ckode.Dapper.Repository.IntegrationTests/Entities/CategoryEntity.cs
@@ -2,7 +2,7 @@
 
 namespace Ckode.Dapper.Repository.IntegrationTests.Entities
 {
-	public record CategoryPrimaryKeyEntity : TableEntity
+	public record CategoryPrimaryKeyEntity(int CategoryId) : TableEntity
 	{
 		[PrimaryKeyColumn(true, "CategoryID")]
 		public int CategoryId { get; init; }
