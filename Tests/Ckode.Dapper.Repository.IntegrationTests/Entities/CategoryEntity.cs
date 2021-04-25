36ddbad301ca7bc981a846041012c2f21e1a3602
Steffen Skov <steffen@ckode.dk>
2021-04-25 21:49:27 +0200

    Fix for wrongly committed file..

--- Tests/Ckode.Dapper.Repository.IntegrationTests/Entities/CategoryEntity.cs
+++ Tests/Ckode.Dapper.Repository.IntegrationTests/Entities/CategoryEntity.cs
@@ -2,7 +2,7 @@
 
 namespace Ckode.Dapper.Repository.IntegrationTests.Entities
 {
-	public record CategoryPrimaryKeyEntity(int CategoryId) : TableEntity
+	public record CategoryPrimaryKeyEntity() : TableEntity
 	{
 		[PrimaryKeyColumn(true, "CategoryID")]
 		public int CategoryId { get; init; }
