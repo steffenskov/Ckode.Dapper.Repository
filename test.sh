8503d161b27efc281ccf596cd3b590b2554f9ba8
Steffen Skov <steffen@ckode.dk>
2021-04-26 20:20:54 +0200

    Changed test script to strip old coverage files prior to running test.

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+
+rm -rf Tests/Ckode.Dapper.Repository.IntegrationTests/TestResults
+rm -rf Tests/Ckode.Dapper.Repository.UnitTests/TestResults
 dotnet test --collect:"XPlat Code Coverage" --settings coverlet.runsettings
